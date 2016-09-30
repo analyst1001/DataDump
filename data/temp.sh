4ea13569c6cc758a052e6d558083019e1fb90775
unown <lovepreetm1992@gmail.com>
2016-09-29 20:48:54 -0400

    Squash all placemarks into a single file

--- data/temp.sh
+++ data/temp.sh
@@ -1,17 +1,16 @@
-for file in `ls -1 states`
-do
-    state=`echo $file |cut -d'.' -f1`
     echo '<Document>
       <Style id="bluepoly">
               <PolyStyle>
                 <color>ffff0000</color>
               </PolyStyle>
-         </Style>
-        <Placemark>
+         </Style>' > tempfile
+for file in `ls -1 states`
+do
+    state=`echo $file |cut -d'.' -f1`
+    echo '<Placemark>
         <name>'$state'</name>
-        <styleUrl>#bluepoly</styleUrl>' > tempfile
+        <styleUrl>#bluepoly</styleUrl>' >> tempfile
     cat states/$file >> tempfile
-    echo '</Placemark>
-        </Document>' >> tempfile
-    cat tempfile > data2/$file
+    echo '</Placemark>' >> tempfile
 done
+echo '</Document>' >> tempfile
